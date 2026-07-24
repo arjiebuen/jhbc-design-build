6a78ea6220ea481a38398b3b72730bb9ad2dff75
Arjie Buen <75046487+arjiebuen@users.noreply.github.com>
2026-07-24 12:02:09 +0800

    Update Supabase admin client

--- lib/supabase-admin.ts
+++ lib/supabase-admin.ts
@@ -1,9 +1,4 @@
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log(
-    "SERVICE KEY EXISTS:",
-    !(process.env.SUPABASE_SERVICE_ROLE_KEY == null)
-);
+
 
 
 import { createClient } from "@supabase/supabase-js";
